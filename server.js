e070e21192c75d8dbf2fdb2f0d35a2f9bb7751b4
Gregor MacLennan <gmaclennan@digital-democracy.org>
2016-03-02 18:35:38 -0800

    Trust proxy over local and private IP networks (fixes #1)

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ process.env.ALIASES = JSON.stringify(require('./alias-config.json'))
 process.env.VHOSTS = JSON.stringify(require('./vhost-config.json'))
 
 var app = require('simple-odk')
+app.set('trust proxy', ['loopback', 'linklocal', 'uniquelocal'])
 
 // Start server
 var port = process.env.PORT || 8080
